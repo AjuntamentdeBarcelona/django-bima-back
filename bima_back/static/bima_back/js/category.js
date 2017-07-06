2412c0ed550c2cecf6647dba04cac2b157c9d7c8
Daniel Sastre <dsastre@apsl.net>
2017-07-06 16:26:17 +0200

    WIP: Show categories via AJAX

--- bima_back/static/bima_back/js/category.js
+++ bima_back/static/bima_back/js/category.js
@@ -5,43 +5,50 @@ $(function() {
 
   $('div.list-categories').on('click', '.eye', function(e){
     const btn = $(this)
-    if (btn.data('children') === "0") {
+
+    if (!btn.data('children')) {
       return false
     }
+
     if (!btn.data('loaded')) {
       $.ajax({
         url: categoryURLs.list, // from bima_back/categories/category.html
         data: {"parent": btn.data("categoryid")},
         method: 'GET',
       }).done(function(data) {
-        console.debug(data)  // TODO: delete me
         let html = ''
         for (let category of data.results) {
           html += categoryHTML(category)
         }
+
         insertCategories(btn, html)
+
+        if (!btn.data('isroot')) {
+          $(`#subcategories-${btn.data('categoryid')}`).collapse('toggle')
+        }
+
         btn.data('loaded', true)
       })
+    } else {
+      if (!btn.data('isroot')) {
+        $(`#subcategories-${btn.data('categoryid')}`).collapse('toggle')
+      }
     }
   })
 })
 
 function categoryHTML(category) {
-  let html = `<li>${category.name}`
-
-  if (category.extra_info.children) {
-    html += `
-      <span class="pull-right">
-        <button type="button"
-            class="btn btn-transparent btn-sm eye"
-            style="background: transparent; margin-top: -10px;"
-            data-widget="collapse"
-            data-children="${category.extra_info.children}"
-            data-categoryid="${category.id}">
-          <span>${category.extra_info.children}</span>&nbsp;<i class="fa fa-eye"></i>
-        </button>
-      </span>`
-  }
+  let html = `<li>
+    ${category.name}
+    <span class="pull-right">
+      <button type="button"
+          class="btn btn-transparent btn-sm eye"
+          style="background: transparent; margin-top: -8px;"
+          data-children="${category.extra_info.children}"
+          data-categoryid="${category.id}">
+        <span>${category.extra_info.children}</span>&nbsp;<i class="fa fa-eye"></i>
+      </button>
+    </span>`
 
   if (category.permissions.write) {
     html += `
@@ -62,6 +69,10 @@ function insertCategories(categoryButton, categoriesHTML) {
   if (!categoriesHTML) {
     return
   }
-  const html = `<ul>${categoriesHTML}</ul>`
-  categoryButton.closest('div.panel-heading').next(".panel-body").html(html)
+  const html = `<ul id="subcategories-${categoryButton.data('categoryid')}">${categoriesHTML}</ul>`
+  if (categoryButton.data('isroot')) {
+    categoryButton.closest('.panel-heading').next(".panel-body").html(html)
+  } else {
+    categoryButton.closest('li').append(html)
+  }
 }
